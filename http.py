d29a96284c7c73543ad5931c9e8e6150d2f23b19
root <wufuheng@gmail.com>
2013-12-04 08:52:51 -0800

    first commit

---
+++ http.py
@@ -0,0 +1,61 @@
+#!/usr/local/python3/bin/python3
+import socket, select
+import signal
+import sys
+
+def handleINT(signum,frame):
+  print("signum:%d,frame:%s" % (signum,frame))
+  print( "---- good bye ----")
+  sys.exit(0)
+
+signal.signal(signal.SIGINT,handleINT)
+
+EOL1 = b'\n\n'
+EOL2 = b'\n\r\n'
+response  = b'HTTP/1.0 200 OK\r\nDate: Mon, 1 Jan 1996 01:01:01 GMT\r\n'
+response += b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
+response += b'Hello, world!'
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind(('0.0.0.0', 8080))
+serversocket.listen(1)
+serversocket.setblocking(0)
+
+epoll = select.epoll()
+epoll.register(serversocket.fileno(), select.EPOLLIN)
+
+try:
+   connections = {}; requests = {}; responses = {}
+   while True:
+      events = epoll.poll(1)
+      for fileno, event in events:
+         print(fileno,event)
+         if fileno == serversocket.fileno():
+            connection, address = serversocket.accept()
+            print("connection:%s address:%s" % (connection, address))
+            connection.setblocking(0)
+            print("connection.fileno:%d" % (connection.fileno()))
+            epoll.register(connection.fileno(), select.EPOLLIN)
+            connections[connection.fileno()] = connection
+            requests[connection.fileno()] = b''
+            responses[connection.fileno()] = response
+         elif event & select.EPOLLIN:
+            requests[fileno] += connections[fileno].recv(1024)
+            if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
+               epoll.modify(fileno, select.EPOLLOUT)
+               print('-'*40 + '\n' + requests[fileno].decode()[:-2])
+         elif event & select.EPOLLOUT:
+            byteswritten = connections[fileno].send(responses[fileno])
+            responses[fileno] = responses[fileno][byteswritten:]
+            if len(responses[fileno]) == 0:
+               epoll.modify(fileno, 0)
+               connections[fileno].shutdown(socket.SHUT_RDWR)
+         elif event & select.EPOLLHUP:
+            epoll.unregister(fileno)
+            connections[fileno].close()
+            del connections[fileno]
+finally:
+   epoll.unregister(serversocket.fileno())
+   epoll.close()
+   serversocket.close()
