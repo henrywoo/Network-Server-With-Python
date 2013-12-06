4f71ccc6bce68cad4e8b2894a7a6679dcfceea74
root <wufuheng@gmail.com>
2013-12-06 06:08:50 -0800

    multiprocess epoll server echo-mt-reactor.py

--- echo-client.py
+++ echo-client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Client side: use sockets to send data to the server, and print server's
+Client side: use sockets to send data to the server, and #print server's
 reply to each message line; 'localhost' means that the server is running
 on the same machine as the client, which lets us test client and server
 on one machine;  to test over the Internet, run a server on a remote
@@ -11,12 +11,13 @@ for the standard socket calls available in the system's C library;
 
 import sys
 from socket import *              # portable socket interface plus constants
+import datetime
 
 
 serverHost = 'c58'          # server name, or: 'starship.python.net'
 serverPort = 2007                # non-reserved port used by the server
 
-message = [b'Hello network world']*10          # default text to send to server
+message = [b'H'*1]*50000          # default text to send to server
                                             # requires bytes: b'' or str,encode()
 if len(sys.argv) > 1:
     serverHost = sys.argv[1]                # server from cmd line arg 1
@@ -26,10 +27,25 @@ if len(sys.argv) > 1:
 #sockobj = socket(AF_INET, SOCK_STREAM)      # make a TCP/IP socket object
 #sockobj.connect((serverHost, serverPort))   # connect to server machine + port
 
+t1 = datetime.datetime.now()
 for line in message:
     sockobj = socket(AF_INET, SOCK_STREAM)      # make a TCP/IP socket object
     sockobj.connect((serverHost, serverPort))   # connect to server machine + port
+    sockobj.setblocking(0)
     sockobj.send(line)                      # send line to server over socket
-    data = sockobj.recv(1024)               # receive line from server: up to 1k
-    #print('Client received:', data)         # bytes are quoted, was `x`, repr(x)
+    leng=0
+    data=''
+    #print(len(line))
+    while leng<len(line):
+        try:
+            tmp = sockobj.recv(1024)               # receive line from server: up to 1k
+        except:continue
+        leng += len(tmp)
+        data += tmp
+    #print(len(data))
+    ##print('Client received:', data)         # bytes are quoted, was `x`, repr(x)
     sockobj.close()                             # close socket to send eof to server
+t2 = datetime.datetime.now()
+c=(t2-t1)
+print(c.seconds,"s")
+print(c.microseconds,"us")
--- echo-mt-reactor.py
+++ echo-mt-reactor.py
@@ -4,56 +4,73 @@ import select, time
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Pool, Process
 from Queue import Queue
-import os
+import os,sys
 
-p = Pool(50)
-def f(x):
-    return x*x
 isblocking = 0
 queuedconnections = 5
 server_socket = None
-ep=None
-q = Queue()
-q2 = Queue()
-
 
 def handle_input(socket, data):
-    print("got data:",data)
-    #todo
-    socket.send(data) # sendall() partial?
+    try:
+        #print("got data:",data)
+        count=0
+        sz=len(data)
+        while (count<sz):
+            sent = socket.send(data) # sendall() partial???
+            #print("sent out %d bytes\n" % sent)
+            count += sent
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
 
-def handle_request(fileno):
-    if event & select.POLLIN:# to be removed
-        #client_socket = connections[fileno]
-        client_socket = q2.get()
-        data = client_socket.recv(4096);
-        #todo
-        #time.sleep(5)
-        if data:
-            handle_input(client_socket, data)
-        else:
-            ep.unregister(fileno)
-            client_socket.close()
-            #del connections[fileno]
-            #del handlers[fileno]
+    except Exception as e:
+        print "pid=",os.getpid(),",error({0}): {1}".format(e.errno, e.strerror), ", Unexpected error:", sys.exc_info()[0]
+        pass
 
-def handle_accept(ep_):
-    (client_socket, client_address) = server_socket.accept()
-    print "got connection from", client_address
-    client_socket.setblocking(isblocking)
-    ep_.register(client_socket.fileno(), select.POLLIN)
-    q2.put(client_socket)
+def SpawnIOProcess(ss):
+    """ss - server socket"""
+    #time.sleep(2)
+    ep = select.epoll()
+    ssno = ss.fileno()
+    ep.register(ssno, select.POLLIN|select.EPOLLET) # edge trigger more effective
+    print("register ",ssno," into interest list of pid=",os.getpid())
+    connections = {}
 
-if __name__=='__main__2':
-    #p.map(f, [1,2,3])
-    pass
-
-def worker():
     while True:
-        fileno = q.get()
-        handle_request(fileno)
-        q.task_done()
-
+        events = ep.poll()  # infinitely waiting
+        for fileno, event in events:
+            if fileno==ssno:
+                try:
+                    (client_socket, client_address) = ss.accept()
+                    #print("pid:",os.getpid())
+                    lno = client_socket.fileno()
+                    #print "got connection from", client_address
+                    client_socket.setblocking(isblocking)
+                    ep.register(lno, select.POLLIN|select.EPOLLET)
+                    #print("register ",lno," into interest list of pid=",os.getpid())
+                    connections[lno] = client_socket
+                except Exception as e:
+                    #print "pid=",os.getpid(),",error({0}): {1}".format(e.errno, e.strerror), ", Unexpected error:", sys.exc_info()[0]
+                    pass
+            else:
+                if event & select.POLLIN:
+                    client_socket = connections[fileno]
+                    try:
+                        tmp=client_socket.recv(1024)
+                        data=''
+                        while tmp:
+                            data += tmp
+                            #print len(data)
+                            tmp=client_socket.recv(1024)#,select.MSG_DONTWAIT)####??
+                    except Exception as e:pass
+                        #print "pid=",os.getpid(),",error({0}): {1}".format(e.errno, e.strerror), ", Unexpected error:", sys.exc_info()[0]
+                if data:
+                    handle_input(client_socket, data)####
+                else:
+                    ep.unregister(fileno)####
+                    #print("unregister ",fileno," into interest list of pid=",os.getpid())
+                    client_socket.close()
+                    del connections[fileno]
+                    #time.sleep(60)
 
 if __name__=='__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,35 +80,11 @@ if __name__=='__main__':
     server_socket.setblocking(isblocking)
 
     #should start multiprocess here!!!
-    pid=os.fork()
-    if(pid==0):#child
-        SpawnIOProcess(server_socket)
-    else:
-        while True:
-            time.sleep(10)
-    ep = select.epoll()
-    ep.register(server_socket.fileno(), select.POLLIN|select.EPOLLET)
-    print (server_socket.fileno())
-    #handlers[server_socket.fileno()] = handle_accept #socket fd -> functor
+    ps=[]
+    for i in xrange(10):# num of process 5
+        ps.append(Process(target=SpawnIOProcess, args=(server_socket,)))
 
-    #pl = Pool(processes=10)
-    p = Process(target=worker,args=())
-
-    #proc 1: epoll
-    sn=server_socket.fileno()
-    while True:
-        events = ep.poll()  # 10 seconds
-        print(events)
-        for fileno, event in events:
-            if(fileno==sn):
-                handle_accept(ep)
-            else:
-                print(fileno, event)
-                q.put(fileno)
-                if not p.is_alive():
-                    p.start()
-                #result = pl.apply_async(worker, [])
-
-
-            ##handlers[fileno](fileno,event)
-            # put into the Queue
+    for p in ps:
+        p.start()
+    for p in ps:
+        p.join()
